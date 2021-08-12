9876c6e1879e718d39d4675447e4a651631b0ae8
fairyinabottle4 <keithlow98@gmail.com>
2021-08-12 22:31:18 +0800

    Assign frequent flyer status to newly added passenger

--- build/src/services/patientService.js
+++ build/src/services/patientService.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const patients_1 = __importDefault(require("../../data/patients"));
 const utils_1 = require("../utils");
 const uuid_1 = require("uuid");
+const types_1 = require("../types");
 const patients = patients_1.default;
 const id = uuid_1.v1();
 const getPatients = () => {
@@ -22,12 +23,35 @@ const getSinglePatient = (id) => {
 };
 const addPatient = (entry) => {
     //at this point, the backend will query database if passenger has past experience 
-    //with the airline. If yes, the previous rating score will be fetched. 
+    //with the airline. If yes, the previous rating score will be fetched. Likewise with
+    //the frequent flyer status
     //For now, this will be a random number
     const rating = Math.floor(Math.random() * 5);
-    const newPatient = Object.assign(Object.assign({ id: id }, entry), { rating });
-    patients.push(newPatient);
-    return newPatient;
+    //For now, frequent flyer status is determined by class of travel. 
+    if (entry.travelClass !== types_1.TravelClass.Economy) {
+        let frequentFlyer;
+        if (entry.travelClass === types_1.TravelClass.First) {
+            frequentFlyer = types_1.FrequentFlyer.Platinum;
+        }
+        else if (entry.travelClass === types_1.TravelClass.Business) {
+            frequentFlyer = types_1.FrequentFlyer.Gold;
+        }
+        else if (entry.travelClass === types_1.TravelClass.PremiumEconomy) {
+            frequentFlyer = types_1.FrequentFlyer.Silver;
+        }
+        else {
+            frequentFlyer = types_1.FrequentFlyer.Silver;
+        }
+        const newPatient = Object.assign(Object.assign({ id: id }, entry), { rating,
+            frequentFlyer });
+        patients.push(newPatient);
+        return newPatient;
+    }
+    else {
+        const newPatient = Object.assign(Object.assign({ id: id }, entry), { rating });
+        patients.push(newPatient);
+        return newPatient;
+    }
 };
 const addEntry = (patientId, entry) => {
     const patient = getSinglePatient(patientId);
--- src/services/patientService.ts
+++ src/services/patientService.ts
@@ -2,7 +2,7 @@ import patientData from '../../data/patients'
 import { censorPatient } from "../utils";
 import {v1 as uuid} from 'uuid'
 import { Patient, CensoredPatient, NewPatientEntry } from '../types'
-import { BaseEntry } from '../types'
+import { BaseEntry, FrequentFlyer, TravelClass } from '../types'
 
 const patients: Array<Patient> = patientData
 
@@ -23,17 +23,39 @@ const getSinglePatient = (id: string): Patient => {
 
 const addPatient = ( entry: NewPatientEntry ): Patient => {
   //at this point, the backend will query database if passenger has past experience 
-  //with the airline. If yes, the previous rating score will be fetched. 
-
+  //with the airline. If yes, the previous rating score will be fetched. Likewise with
+  //the frequent flyer status
   //For now, this will be a random number
   const rating = Math.floor(Math.random() * 5)
-  const newPatient = {
-    id: id,
-    ...entry,
-    rating
-  };
-  patients.push(newPatient);
-  return newPatient;
+  //For now, frequent flyer status is determined by class of travel. 
+  if (entry.travelClass !== TravelClass.Economy) {
+    let frequentFlyer: FrequentFlyer;
+    if (entry.travelClass === TravelClass.First) {
+      frequentFlyer = FrequentFlyer.Platinum;
+    } else if (entry.travelClass === TravelClass.Business) {
+      frequentFlyer = FrequentFlyer.Gold;
+    } else if (entry.travelClass === TravelClass.PremiumEconomy) {
+      frequentFlyer = FrequentFlyer.Silver;
+    } else {
+      frequentFlyer = FrequentFlyer.Silver;
+    }
+    const newPatient = {
+      id: id,
+      ...entry,
+      rating,
+      frequentFlyer
+    };
+    patients.push(newPatient);
+    return newPatient;  
+  } else {
+    const newPatient = {
+      id: id,
+      ...entry,
+      rating
+    };
+    patients.push(newPatient);
+    return newPatient;  
+  }
 }
 
 const addEntry = (patientId: string, entry: BaseEntry): BaseEntry => {
