3e1a22cb6284e6b4e35acde5ecca97ad4f4efe09
fairyinabottle4 <keithlow98@gmail.com>
2021-08-13 12:31:31 +0800

    heroku port

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ app.use(cors())
 app.use(express.json());
 app.use(express.static('build/frontend'))
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
